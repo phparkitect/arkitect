86c33b4b23b18f0aee7cbd3be2b88e33c033d7c6
pfazzi <patrick91@live.it>
2020-07-10 13:12:39 +0200

    Add unit test for Assert class

---
+++ tests/unit/AssertTest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace ArkitectTests\unit;
+
+use Arkitect\ArchViolationsException;
+use Arkitect\Assert;
+use Arkitect\ClassSet;
+use Arkitect\Rules\ArchRuleGivenClasses;
+use Arkitect\Rules\ViolationsStore;
+use PHPUnit\Framework\TestCase;
+
+class AssertTest extends TestCase
+{
+    public function test_it_checks_the_rule_against_the_class_set_and_throws_exception(): void
+    {
+        $classSet = $this->prophesize(ClassSet::class);
+        $rule = $this->prophesize(ArchRuleGivenClasses::class);
+
+        $rule->check($classSet)->shouldBeCalled();
+
+        $rule
+            ->getViolations()
+            ->willReturn(new ViolationsStore('Random violation 1', 'Random violation 2'));
+
+        $assert = new Assert($classSet->reveal(), $rule->reveal());
+
+        self::expectException(ArchViolationsException::class);
+
+        $assert->run();
+    }
+}
