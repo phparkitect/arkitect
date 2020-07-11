897b97eee521e927f096a0a3c08ef57883d50e93
Pietro Campagnano <fain182@gmail.com>
2020-07-11 15:39:53 +0200

    code style fix

--- tests/unit/Analyzer/PatternStringTest.php
+++ tests/unit/Analyzer/PatternStringTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace ArkitectTests\unit\Analyzer;
 
@@ -7,23 +8,24 @@ use PHPUnit\Framework\TestCase;
 
 class PatternStringTest extends TestCase
 {
-
-    public function test_it_works_for_simple_strings() {
+    public function test_it_works_for_simple_strings()
+    {
         $pattern = new PatternString("Example");
         $this->assertTrue($pattern->matches("Example"));
         $this->assertFalse($pattern->matches("Something else"));
     }
 
-    public function test_wildcard_is_for_alphanumeric() {
+    public function test_wildcard_is_for_alphanumeric()
+    {
         $pattern = new PatternString("SoThisIsAnExample");
         $this->assertTrue($pattern->matches("*This*"));
         $this->assertFalse($pattern->matches("This*"));
     }
 
-    public function test_double_wildcard_accepts_every_character_and_space() {
+    public function test_double_wildcard_accepts_every_character_and_space()
+    {
         $pattern = new PatternString("So This Is An Example");
         $this->assertTrue($pattern->matches("**This**"));
         $this->assertFalse($pattern->matches("*This*"));
     }
-
 }
--- tests/unit/Costraints/HaveNameMatchingTest.php
+++ tests/unit/Costraints/HaveNameMatchingTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace ArkitectTests\unit\Costraints;
 
@@ -8,20 +9,20 @@ use PHPUnit\Framework\TestCase;
 
 class HaveNameMatchingTest extends TestCase
 {
-
-    public function testHappyPath() {
+    public function testHappyPath()
+    {
         $constraint = new HaveNameMatching("**Class");
 
         $goodClass = ClassDescription::build('\App\MyClass', 'App')->get();
         $this->assertFalse($constraint->isViolatedBy($goodClass));
     }
 
-    public function testWrongName() {
+    public function testWrongName()
+    {
         $constraint = new HaveNameMatching("**GoodName**");
 
         $badClass = ClassDescription::build('\App\BadNameClass', 'App')->get();
         $this->assertTrue($constraint->isViolatedBy($badClass));
         $this->assertEquals('\App\BadNameClass have name that doesn\'t match **GoodName**', $constraint->getViolationError($badClass));
     }
-
 }
