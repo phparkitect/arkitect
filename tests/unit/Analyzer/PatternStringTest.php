59f73bcae4181a66a627cd40dbd4d859b7ad0343
Pietro Campagnano <fain182@gmail.com>
2020-07-11 15:28:50 +0200

    adds unit test

---
+++ tests/unit/Analyzer/PatternStringTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ArkitectTests\unit\Analyzer;
+
+use Arkitect\Analyzer\PatternString;
+use PHPUnit\Framework\TestCase;
+
+class PatternStringTest extends TestCase
+{
+
+    public function test_it_works_for_simple_strings() {
+        $pattern = new PatternString("Example");
+        $this->assertTrue($pattern->matches("Example"));
+        $this->assertFalse($pattern->matches("Something else"));
+    }
+
+    public function test_wildcard_is_for_alphanumeric() {
+        $pattern = new PatternString("SoThisIsAnExample");
+        $this->assertTrue($pattern->matches("*This*"));
+        $this->assertFalse($pattern->matches("This*"));
+    }
+
+    public function test_double_wildcard_accepts_every_character_and_space() {
+        $pattern = new PatternString("So This Is An Example");
+        $this->assertTrue($pattern->matches("**This**"));
+        $this->assertFalse($pattern->matches("*This*"));
+    }
+
+}
