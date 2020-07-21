40abca33a357bd653ce4c192eb913d10ad8e88e0
Alessandro Minoccheri <alessandro.minoccheri@ideato.info>
2020-07-21 08:47:29 +0200

    added other tests

---
+++ tests/unit/Specs/DependOnNamespaceSpecTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace ArkitectTests\unit\Specs;
+
+
+use Arkitect\Analyzer\ClassDependency;
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Analyzer\FullyQualifiedClassName;
+use Arkitect\Specs\DependOnNamespaceSpec;
+use PHPUnit\Framework\TestCase;
+
+class DependOnNamespaceSpecTest extends TestCase
+{
+    public function test_it_should_return_false_if_class_not_depend_on_namespace(): void
+    {
+        $dependOnNamespaceSpec = new DependOnNamespaceSpec('foo');
+
+        $classDescription = new ClassDescription(
+            '/path',
+            FullyQualifiedClassName::fromString('HappyIsland'),
+            [],
+            []
+        );
+
+        $this->assertFalse($dependOnNamespaceSpec->apply($classDescription));
+    }
+
+    public function test_it_should_return_true_if_class_depend_on_namespace(): void
+    {
+        $dependOnNamespaceSpec = new DependOnNamespaceSpec('foo');
+        $classDependency = new ClassDependency('foo\OtherClass', 100);
+
+        $classDescription = new ClassDescription(
+            '/foo',
+            FullyQualifiedClassName::fromString('foo\HappyIsland'),
+            [
+                $classDependency
+            ],
+            []
+        );
+
+        $this->assertTrue($dependOnNamespaceSpec->apply($classDescription));
+    }
+}
---
+++ tests/unit/Specs/ImplementInterfaceSpecTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace ArkitectTests\unit\Specs;
+
+
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Analyzer\FullyQualifiedClassName;
+use Arkitect\Specs\ImplementInterfaceSpec;
+use PHPUnit\Framework\TestCase;
+
+class ImplementInterfaceSpecTest extends TestCase
+{
+    public function test_it_should_return_false_if_not_implement_interface(): void
+    {
+        $haveNameMatching = new ImplementInterfaceSpec('MyInterface');
+
+        $classDescription = new ClassDescription(
+            '/path',
+            FullyQualifiedClassName::fromString('HappyIsland'),
+            [],
+            [
+                FullyQualifiedClassName::fromString('AnotherInterface'),
+            ]
+        );
+
+        $this->assertFalse($haveNameMatching->apply($classDescription));
+    }
+
+    public function test_it_should_return_true_if_implement_interface(): void
+    {
+        $haveNameMatching = new ImplementInterfaceSpec('MyInterface');
+
+        $classDescription = new ClassDescription(
+            '/path',
+            FullyQualifiedClassName::fromString('HappyIsland'),
+            [],
+            [
+                FullyQualifiedClassName::fromString('MyInterface'),
+            ]
+        );
+
+        $this->assertTrue($haveNameMatching->apply($classDescription));
+    }
+}
