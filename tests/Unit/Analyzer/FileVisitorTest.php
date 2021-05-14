8c96e9bc5a9c3264e99fd3048448374ba1b5f7a0
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2021-05-14 21:18:37 +0200

    Fixing bug into fileVisitor with new anonymous class (#122)
    * fixing bug into fileVisitor with new anonymous class

--- tests/Unit/Analyzer/FileVisitorTest.php
+++ tests/Unit/Analyzer/FileVisitorTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Arkitect\Tests\Unit\Analyzer;
 
+use Arkitect\Analyzer\ClassDependency;
 use Arkitect\Analyzer\ClassDescription;
 use Arkitect\Analyzer\FileParser;
 use Arkitect\Analyzer\FileParserFactory;
@@ -37,5 +38,61 @@ EOF;
         self::assertCount(2, $cd);
         self::assertInstanceOf(ClassDescription::class, $cd[0]);
         self::assertInstanceOf(ClassDescription::class, $cd[1]);
+    }
+
+    public function test_should_create_a_class_description_and_parse_anonymous_class(): void
+    {
+        $code = <<< 'EOF'
+<?php
+
+namespace Root\Namespace1;
+
+use Root\Namespace2\D;
+
+class Dog implements AnInterface, InterfaceTwo
+{
+    public function foo()
+    {
+        $projector2 = new class() implements Another\ForbiddenInterface
+            {
+                public function applyDummyDomainEvent(int $anInteger): void
+                {
+                }
+
+                public function getEventsTypes(): string
+                {
+                    return "";
+                }
+            };
+
+            $projector = new Proj();
+    }
+}
+
+class Cat implements AnInterface
+{
+
+}
+EOF;
+
+        /** @var FileParser $fp */
+        $fp = FileParserFactory::createFileParser();
+        $fp->parse($code);
+        $cd = $fp->getClassDescriptions();
+
+        self::assertCount(3, $cd);
+        self::assertInstanceOf(ClassDescription::class, $cd[0]);
+        self::assertInstanceOf(ClassDescription::class, $cd[1]);
+
+        $expectedInterfaces = [
+            new ClassDependency('Root\Namespace1\AnInterface', 7),
+            new ClassDependency('\Root\Namespace1\AnInterface', 7),
+            new ClassDependency('Root\Namespace1\InterfaceTwo', 7),
+            new ClassDependency('\Root\Namespace1\InterfaceTwo', 7),
+            new ClassDependency('Root\Namespace1\Another\ForbiddenInterface', 11),
+            new ClassDependency('\Root\Namespace1\Another\ForbiddenInterface', 11),
+        ];
+
+        $this->assertEquals($expectedInterfaces, $cd[0]->getDependencies());
     }
 }
