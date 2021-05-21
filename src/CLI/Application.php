e28ed86d25a37ba336eda832bab6f9ddc099f699
Michele Orselli <michele.orselli@flowing.it>
2021-05-21 16:14:29 +0200

    cs fix

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -1,16 +1,15 @@
 <?php
-
+declare(strict_types=1);
 
 namespace Arkitect\CLI;
-
 
 class Application extends \Symfony\Component\Console\Application
 {
     private static $logo = <<< 'EOD'
-  ____  _   _ ____   _         _    _ _            _   
- |  _ \| | | |  _ \ / \   _ __| | _(_) |_ ___  ___| |_ 
+  ____  _   _ ____   _         _    _ _            _
+ |  _ \| | | |  _ \ / \   _ __| | _(_) |_ ___  ___| |_
  | |_) | |_| | |_) / _ \ | '__| |/ / | __/ _ \/ __| __|
- |  __/|  _  |  __/ ___ \| |  |   <| | ||  __/ (__| |_ 
+ |  __/|  _  |  __/ ___ \| |  |   <| | ||  __/ (__| |_
  |_|   |_| |_|_| /_/   \_\_|  |_|\_\_|\__\___|\___|\__|
 EOD;
 
