e7315cb726c6fe5a49699a68c1576659dd499ce2
Michele Orselli <michele.orselli@flowing.it>
2021-05-21 16:11:47 +0200

    adds ascii art logo and version

---
+++ src/CLI/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Arkitect\CLI;
+
+
+class Application extends \Symfony\Component\Console\Application
+{
+    private static $logo = <<< 'EOD'
+  ____  _   _ ____   _         _    _ _            _   
+ |  _ \| | | |  _ \ / \   _ __| | _(_) |_ ___  ___| |_ 
+ | |_) | |_| | |_) / _ \ | '__| |/ / | __/ _ \/ __| __|
+ |  __/|  _  |  __/ ___ \| |  |   <| | ||  __/ (__| |_ 
+ |_|   |_| |_|_| /_/   \_\_|  |_|\_\_|\__\___|\___|\__|
+EOD;
+
+    public function getLongVersion()
+    {
+        return sprintf("%s\n\n<info>%s</info> version <comment>%s</comment>", self::$logo, $this->getName(), $this->getVersion());
+    }
+}
