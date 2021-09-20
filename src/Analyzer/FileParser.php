a465bb8a4b30d7683c35f9726a3e57eaade40e72
AlessandroMinoccheri <alessandro.minoccheri@ideato.info>
2021-09-20 13:55:33 +0200

    added constant for coding standard

--- src/Analyzer/FileParser.php
+++ src/Analyzer/FileParser.php
@@ -25,7 +25,7 @@ class FileParser implements Parser
 
         $lexer = new Emulative([
             'usedAttributes' => ['comments', 'startLine', 'endLine', 'startTokenPos', 'endTokenPos'],
-             'phpVersion' => phpversion(),
+             'phpVersion' => phpversion('tidy'),
         ]);
 
         $this->parser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7, $lexer);
