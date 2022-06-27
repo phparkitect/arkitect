532d34389f438fba62faa809abdf88288daa4621
Alessandro Minoccheri <alessandro.minoccheri@ideato.info>
2022-06-27 17:59:19 +0200

    added missing tests for expressions classes not covered (#265)

---
+++ tests/Unit/Expressions/ForClasses/DocBlockNotContainsTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Tests\Unit\Expressions\ForClasses;
+
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Analyzer\FullyQualifiedClassName;
+use Arkitect\Expression\ForClasses\DocBlockNotContains;
+use Arkitect\Rules\Violations;
+use PHPUnit\Framework\TestCase;
+
+class DocBlockNotContainsTest extends TestCase
+{
+    public function test_it_should_return_true_if_not_contains_doc_block(): void
+    {
+        $expression = new DocBlockNotContains('anotherDocBlock');
+
+        $classDescription = new ClassDescription(
+            FullyQualifiedClassName::fromString('HappyIsland\Myclass'),
+            [],
+            [],
+            null,
+            false,
+            false,
+            '/**  */myDocBlock with other information'
+        );
+        $because = 'we want to add this rule for our software';
+        $violations = new Violations();
+        $expression->evaluate($classDescription, $violations, $because);
+
+        self::assertEquals(0, $violations->count());
+        self::assertEquals(
+            'should not have a doc block that contains anotherDocBlock because we want to add this rule for our software',
+            $expression->describe($classDescription, $because)->toString()
+        );
+    }
+
+    public function test_it_should_return_false_if_contains_doc_block_without_because(): void
+    {
+        $expression = new DocBlockNotContains('anotherDocBlock');
+
+        $classDescription = new ClassDescription(
+            FullyQualifiedClassName::fromString('HappyIsland\Myclass'),
+            [],
+            [],
+            null,
+            false,
+            false,
+            '/**  */myDocBlock with other information'
+        );
+        $violations = new Violations();
+        $expression->evaluate($classDescription, $violations, '');
+
+        self::assertEquals(0, $violations->count());
+        self::assertEquals(
+            'should not have a doc block that contains anotherDocBlock',
+            $expression->describe($classDescription, '')->toString()
+        );
+    }
+
+    public function test_it_should_return_false_if_contains_doc_block(): void
+    {
+        $expression = new DocBlockNotContains('myDocBlock');
+
+        $classDescription = new ClassDescription(
+            FullyQualifiedClassName::fromString('HappyIsland\Myclass'),
+            [],
+            [],
+            null,
+            false,
+            false,
+            '/**  */myDocBlock with other information'
+        );
+        $because = 'we want to add this rule for our software';
+        $violations = new Violations();
+        $expression->evaluate($classDescription, $violations, $because);
+
+        self::assertEquals(1, $violations->count());
+    }
+}
