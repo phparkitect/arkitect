b41f5776ebee467e919ae3eb58715240a7ce1cf3
AlessandroMinoccheri <alessandro.minoccheri@ideato.info>
2022-08-03 18:33:32 +0200

    bump to version 0.2.27

--- tests/Unit/CLI/VersionTest.php
+++ tests/Unit/CLI/VersionTest.php
@@ -11,6 +11,6 @@ class VersionTest extends TestCase
 {
     public function test_it_should_return_version(): void
     {
-        $this->assertEquals('0.2.26', Version::get());
+        $this->assertEquals('0.2.27', Version::get());
     }
 }
