b68056291fdf93f658f51d91d42eb94b4da63ade
Alessandro Minoccheri <alessandro.minoccheri@ideato.info>
2023-02-19 12:22:26 +0100

    added a test for not extend rule when a class extends another class (#356)

--- tests/Unit/Expressions/ForClasses/NotExtendTest.php
+++ tests/Unit/Expressions/ForClasses/NotExtendTest.php
@@ -34,4 +34,26 @@ class NotExtendTest extends TestCase
         self::assertEquals(1, $violations->count());
         self::assertEquals('should not extend My\BaseClass because we want to add this rule for our software', $violationError);
     }
+
+    public function test_it_should_not_return_violation_error_if_extends_another_class(): void
+    {
+        $notExtend = new NotExtend('My\BaseClass');
+
+        $classDescription = new ClassDescription(
+            FullyQualifiedClassName::fromString('HappyIsland'),
+            [],
+            [],
+            FullyQualifiedClassName::fromString('My\AnotherClass'),
+            false,
+            false,
+            false
+        );
+        $because = 'we want to add this rule for our software';
+        $violationError = $notExtend->describe($classDescription, $because)->toString();
+
+        $violations = new Violations();
+        $notExtend->evaluate($classDescription, $violations, $because);
+
+        self::assertEquals(0, $violations->count());
+    }
 }
