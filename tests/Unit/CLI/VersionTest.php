19aecb5c6eda59a01e81bf57b940075be060ba9b
AlessandroMinoccheri <aminoccheri@inpost.it>
2023-03-12 21:58:00 +0100

    bump to 0.3.17

--- tests/Unit/CLI/VersionTest.php
+++ tests/Unit/CLI/VersionTest.php
@@ -11,6 +11,6 @@ class VersionTest extends TestCase
 {
     public function test_it_should_return_version(): void
     {
-        $this->assertEquals('0.3.16', Version::get());
+        $this->assertEquals('0.3.17', Version::get());
     }
 }
