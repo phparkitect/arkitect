f13a9ed9087a8366fddd83e4ec233f6b40e6bb87
Viktor Szépe <viktor@szepe.net>
2023-03-17 13:36:47 +0000

    Start working on MatchOneOfTheseNames

---
+++ src/Expression/ForClasses/MatchOneOfTheseNames.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Expression\ForClasses;
+
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Analyzer\FullyQualifiedClassName;
+use Arkitect\Expression\Description;
+use Arkitect\Expression\Expression;
+use Arkitect\Rules\Violation;
+use Arkitect\Rules\ViolationMessage;
+use Arkitect\Rules\Violations;
+
+class MatchOneOfTheseNames implements Expression
+{
+    /** @var string */
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function describe(ClassDescription $theClass, string $because): Description
+    {
+        return new Description("should have a name that matches {$this->name}", $because);
+    }
+
+    public function evaluate(ClassDescription $theClass, Violations $violations, string $because): void
+    {
+        $fqcn = FullyQualifiedClassName::fromString($theClass->getFQCN());
+        if (!$fqcn->classMatches($this->name)) {
+            $violation = Violation::create(
+                $theClass->getFQCN(),
+                ViolationMessage::selfExplanatory($this->describe($theClass, $because))
+            );
+            $violations->add($violation);
+        }
+    }
+}
