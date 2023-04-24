c5fe234122412370cda7a18b39015167750dd9ba
Krystian Marcisz <simivar@gmail.com>
2023-04-24 12:37:07 +0200

    Skip isFinal check on traits & add IsTrait and IsNotTrait rules (#382)
    * Skip isFinal check on traits

---
+++ tests/Unit/Expressions/ForClasses/IsNotTraitTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Tests\Unit\Expressions\ForClasses;
+
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Analyzer\FullyQualifiedClassName;
+use Arkitect\Expression\ForClasses\IsNotTrait;
+use Arkitect\Rules\Violations;
+use PHPUnit\Framework\TestCase;
+
+class IsNotTraitTest extends TestCase
+{
+    public function test_it_should_return_violation_error(): void
+    {
+        $isFinal = new IsNotTrait();
+        $classDescription = new ClassDescription(
+            FullyQualifiedClassName::fromString('HappyIsland'),
+            [],
+            [],
+            null,
+            false,
+            false,
+            false,
+            true,
+            false
+        );
+        $because = 'we want to add this rule for our software';
+        $violationError = $isFinal->describe($classDescription, $because)->toString();
+
+        $violations = new Violations();
+        $isFinal->evaluate($classDescription, $violations, $because);
+        self::assertNotEquals(0, $violations->count());
+
+        $this->assertEquals('HappyIsland should not be trait because we want to add this rule for our software', $violationError);
+    }
+
+    public function test_it_should_return_true_if_is_not_trait(): void
+    {
+        $isFinal = new IsNotTrait();
+        $classDescription = new ClassDescription(
+            FullyQualifiedClassName::fromString('HappyIsland'),
+            [],
+            [],
+            null,
+            false,
+            false,
+            false,
+            false,
+            false
+        );
+        $because = 'we want to add this rule for our software';
+        $violations = new Violations();
+        $isFinal->evaluate($classDescription, $violations, $because);
+        self::assertEquals(0, $violations->count());
+    }
+}
