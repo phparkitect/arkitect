2583b859a9b5021d25febafd11718d45e144b7ec
Julien Ravia <julien.ravia@gmail.com>
2023-05-08 18:50:55 +0200

    Add IsEnum && IsNotEnum expressions

---
+++ src/Expression/ForClasses/IsEnum.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Arkitect\Expression\ForClasses;
+
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Expression\Description;
+use Arkitect\Expression\Expression;
+use Arkitect\Rules\Violation;
+use Arkitect\Rules\ViolationMessage;
+use Arkitect\Rules\Violations;
+
+class IsEnum implements Expression
+{
+    public function describe(ClassDescription $theClass, string $because): Description
+    {
+        return new Description("{$theClass->getName()} should be an enum", $because);
+    }
+
+    public function evaluate(ClassDescription $theClass, Violations $violations, string $because): void
+    {
+        if ($theClass->isEnum()) {
+            return;
+        }
+
+        $violation = Violation::create(
+            $theClass->getFQCN(),
+            ViolationMessage::selfExplanatory($this->describe($theClass, $because))
+        );
+
+        $violations->add($violation);
+    }
+}
