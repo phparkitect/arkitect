06927708fe4d4c5ef940bd7606f335c8552a64b0
Herberto Graca <herberto.graca@gmail.com>
2023-09-10 14:09:49 +0200

    Create a IsA expression
    This will allow for testing if a class extends or
    implements another code unit, anywhere in the inheritance tree.

---
+++ src/Expression/ForClasses/IsA.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Expression\ForClasses;
+
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Expression\Description;
+use Arkitect\Expression\Expression;
+use Arkitect\Rules\Violation;
+use Arkitect\Rules\ViolationMessage;
+use Arkitect\Rules\Violations;
+
+final class IsA implements Expression
+{
+    /** @var class-string[] */
+    private $allowedFqcnList;
+
+    /**
+     * @param class-string ...$allowedFqcnList
+     */
+    public function __construct(string ...$allowedFqcnList)
+    {
+        $this->allowedFqcnList = $allowedFqcnList;
+    }
+
+    public function describe(ClassDescription $theClass, string $because): Description
+    {
+        $allowedFqcnList = implode(', ', $this->allowedFqcnList);
+
+        return new Description("should inherit from one of: $allowedFqcnList", $because);
+    }
+
+    public function evaluate(ClassDescription $theClass, Violations $violations, string $because): void
+    {
+        if (!$this->isA($theClass, ...$this->allowedFqcnList)) {
+            $violation = Violation::create(
+                $theClass->getFQCN(),
+                ViolationMessage::selfExplanatory($this->describe($theClass, $because))
+            );
+
+            $violations->add($violation);
+        }
+    }
+
+    /**
+     * @param class-string ...$allowedFqcnList
+     */
+    private function isA(ClassDescription $theClass, string ...$allowedFqcnList): bool
+    {
+        foreach ($allowedFqcnList as $allowedFqcn) {
+            if (is_a($theClass->getFQCN(), $allowedFqcn, true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
