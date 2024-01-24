80f42bd8d1b1450c3f79589c5e1e374fffddb085
Pietro Campagnano <hello@pietro.camp>
2024-01-24 15:48:57 +0100

    Bump to 0.3.29

--- tests/Unit/CLI/VersionTest.php
+++ tests/Unit/CLI/VersionTest.php
@@ -11,6 +11,6 @@ class VersionTest extends TestCase
 {
     public function test_it_should_return_version(): void
     {
-        $this->assertEquals('0.3.28', Version::get());
+        $this->assertEquals('0.3.29', Version::get());
     }
 }
