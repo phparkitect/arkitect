fe510161a5a5af1e068be629ab8df842a444f823
sebastianstucke87 <sebastianstucke87@gmail.com>
2024-03-31 00:26:42 +0100

    feat: add "andShould" (#326)

--- src/Rules/DSL/BecauseParser.php
+++ src/Rules/DSL/BecauseParser.php
@@ -3,7 +3,11 @@ declare(strict_types=1);
 
 namespace Arkitect\Rules\DSL;
 
+use Arkitect\Expression\Expression;
+
 interface BecauseParser
 {
     public function because(string $reason): ArchRule;
+
+    public function andShould(Expression $expression): BecauseParser;
 }
