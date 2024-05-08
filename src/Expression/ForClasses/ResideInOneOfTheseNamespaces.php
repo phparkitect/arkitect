2631ba6112d29872b71b9aad9edd02125d072092
Marcin Michalski <evulmastah@gmail.com>
2024-05-08 21:56:48 +0100

    Reindex namespaces

--- src/Expression/ForClasses/ResideInOneOfTheseNamespaces.php
+++ src/Expression/ForClasses/ResideInOneOfTheseNamespaces.php
@@ -18,7 +18,7 @@ class ResideInOneOfTheseNamespaces implements Expression
 
     public function __construct(string ...$namespaces)
     {
-        $this->namespaces = array_unique($namespaces);
+        $this->namespaces = array_values(array_unique($namespaces));
     }
 
     public function describe(ClassDescription $theClass, string $because): Description
