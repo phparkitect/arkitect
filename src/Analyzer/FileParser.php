2eb54c7bbd8cedb901eedd243ccd21dee27c3146
simivar <simivar@gmail.com>
2025-01-31 03:00:38 +0100

    cs fixer

--- src/Analyzer/FileParser.php
+++ src/Analyzer/FileParser.php
@@ -28,7 +28,7 @@ class FileParser implements Parser
         NodeTraverser $traverser,
         FileVisitor $fileVisitor,
         NameResolver $nameResolver,
-        TargetPhpVersion $targetPhpVersion
+        TargetPhpVersion $targetPhpVersion,
     ) {
         $this->fileVisitor = $fileVisitor;
         $this->parsingErrors = [];
