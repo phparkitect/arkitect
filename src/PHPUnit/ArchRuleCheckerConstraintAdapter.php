c0a9af951449ab4f16f123f8e49a94e923dae25e
Michele Orselli <michele.orselli@gmail.com>
2025-02-11 23:42:51 +0100

    yupx3

--- src/PHPUnit/ArchRuleCheckerConstraintAdapter.php
+++ src/PHPUnit/ArchRuleCheckerConstraintAdapter.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Arkitect\PHPUnit;
@@ -10,11 +11,17 @@ use Arkitect\ClassSetRules;
 use Arkitect\CLI\Progress\VoidProgress;
 use Arkitect\CLI\Runner;
 use Arkitect\CLI\TargetPhpVersion;
-use Arkitect\Rules\ArchRule;
 use Arkitect\Rules\ParsingErrors;
 use Arkitect\Rules\Violations;
 use PHPUnit\Framework\Constraint\Constraint;
 
+/**
+ * @psalm-suppress UndefinedClass
+ *
+ * Since we declared phpunit as a dev dependency we cannot be sure the class PHPUnit\Framework\Constraint\Constraint
+ * will be available at runtime. Given arkitect will be used in a development environment, we can ignore this but it
+ * would be nice to have a way to check if the class is available at runtime.
+ */
 class ArchRuleCheckerConstraintAdapter extends Constraint
 {
     /** @var ClassSet */
@@ -47,8 +54,10 @@ class ArchRuleCheckerConstraintAdapter extends Constraint
         return 'satisfies all architectural constraints';
     }
 
-    protected function matches(/** @var $rule ArchRule */ $other): bool
-    {
+    protected function matches(
+        /** @var $rule ArchRule */
+        $other
+    ): bool {
         $this->runner->check(
             ClassSetRules::create($this->classSet, $other),
             new VoidProgress(),
