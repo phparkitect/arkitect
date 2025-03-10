bb5a7e7ffed8b8fd725ef7fc7b9b2f28c8120a5f
Michele Orselli <michele.orselli@gmail.com>
2025-03-10 21:43:34 +0100

    fixes implements and extends rules (#456)

--- src/Expression/ForClasses/NotImplement.php
+++ src/Expression/ForClasses/NotImplement.php
@@ -27,6 +27,11 @@ class NotImplement implements Expression
         return new Description("should not implement {$this->interface}", $because);
     }
 
+    public function appliesTo(ClassDescription $theClass): bool
+    {
+        return !($theClass->isInterface() || $theClass->isTrait());
+    }
+
     public function evaluate(ClassDescription $theClass, Violations $violations, string $because): void
     {
         if ($theClass->isInterface() || $theClass->isTrait()) {
