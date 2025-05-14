a8e518b0b0a2eef6b8cc42c76c4f699d3f6d1739
Michele Orselli <michele.orselli@gmail.com>
2025-05-14 18:15:43 +0200

    .

--- src/Analyzer/Docblock.php
+++ src/Analyzer/Docblock.php
@@ -6,8 +6,10 @@ namespace Arkitect\Analyzer;
 
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ReturnTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
 use PHPStan\PhpDocParser\Ast\Type\ArrayTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\GenericTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\TypeNode;
 
 class Docblock
@@ -41,8 +43,37 @@ class Docblock
         return array_filter($returnTypes);
     }
 
+    public function getVarTagTypes(): array
+    {
+        $varTypes = array_map(
+            fn (VarTagValueNode $varTag) => $this->getType($varTag->type),
+            $this->phpDocNode->getVarTagValues()
+        );
+
+        // remove null values
+        return array_filter($varTypes);
+    }
+
+    public function getDoctrineLikeAnnotationTypes(): array
+    {
+        $doctrineAnnotations = [];
+
+        foreach ($this->phpDocNode->getTags() as $tag) {
+            if ('@' === $tag->name[0] && !str_contains($tag->name, '@var')) {
+                $doctrineAnnotations[] = str_replace('@', '', $tag->name);
+            }
+        }
+
+        return $doctrineAnnotations;
+    }
+
     private function getType(TypeNode $typeNode): ?string
     {
+        if ($typeNode instanceof IdentifierTypeNode) {
+            // this handles ClassName
+            return $typeNode->name;
+        }
+
         if ($typeNode instanceof GenericTypeNode) {
             // this handles list<ClassName>
             if (1 === \count($typeNode->genericTypes)) {
--- tests/Unit/Analyzer/DocblockParserTest.php
+++ tests/Unit/Analyzer/DocblockParserTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Arkitect\Tests\Unit\Analyzer;
 
-use Arkitect\Analyzer\Docblock;
 use Arkitect\Analyzer\DocblockParserFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -24,20 +23,17 @@ class DocblockParserTest extends TestCase
              * @param int $aValue
              * @param MyPlainDto $plainDto
              */
-        }
         PHP;
 
-        $phpdoc = $parser->parse($code);
-
-        $db = new Docblock($phpdoc);
+        $db = $parser->parse($code);
 
         self::assertEquals('MyDto', $db->getParamTagTypesByName('$dtoList'));
         self::assertEquals('MyOtherDto', $db->getParamTagTypesByName('$dtoList2'));
         self::assertEquals('ValueObject', $db->getParamTagTypesByName('$voList'));
         self::assertEquals('User', $db->getParamTagTypesByName('$user'));
 
-        self::assertNull($db->getParamTagTypesByName('$aValue'));
-        self::assertNull($db->getParamTagTypesByName('$plainDto'));
+        self::assertEquals('int', $db->getParamTagTypesByName('$aValue'));
+        self::assertEquals('MyPlainDto', $db->getParamTagTypesByName('$plainDto'));
     }
 
     public function test_it_should_extract_return_type_from_return_tag(): void
@@ -53,18 +49,70 @@ class DocblockParserTest extends TestCase
              * @return int
              * @return MyPlainDto
              */
-        }
         PHP;
 
-        $phpdoc = $parser->parse($code);
-
-        $db = new Docblock($phpdoc);
+        $db = $parser->parse($code);
 
         $returnTypes = $db->getReturnTagTypes();
-        self::assertCount(4, $returnTypes);
+        self::assertCount(6, $returnTypes);
         self::assertEquals('MyDto', $returnTypes[0]);
         self::assertEquals('MyOtherDto', $returnTypes[1]);
         self::assertEquals('ValueObject', $returnTypes[2]);
         self::assertEquals('User', $returnTypes[3]);
+        self::assertEquals('int', $returnTypes[4]);
+        self::assertEquals('MyPlainDto', $returnTypes[5]);
+    }
+
+    public function test_it_should_extract_types_from_var_tag(): void
+    {
+        $parser = DocblockParserFactory::create();
+
+        $code = <<< 'PHP'
+            /**
+             * @var MyDto[] $dtoList
+             * @var list<MyOtherDto> $dtoList2
+             * @var array<int, ValueObject> $voList
+             * @var array<User> $user
+             * @var int $aValue
+             * @var MyPlainDto $plainDto
+             */
+        PHP;
+
+        $db = $parser->parse($code);
+
+        $varTags = $db->getVarTagTypes();
+        self::assertCount(6, $varTags);
+        self::assertEquals('MyDto', $varTags[0]);
+        self::assertEquals('MyOtherDto', $varTags[1]);
+        self::assertEquals('ValueObject', $varTags[2]);
+        self::assertEquals('User', $varTags[3]);
+        self::assertEquals('int', $varTags[4]);
+        self::assertEquals('MyPlainDto', $varTags[5]);
+    }
+
+    public function test_it_should_extract_doctrine_like_annotations(): void
+    {
+        $parser = DocblockParserFactory::create();
+
+        $code = <<< 'PHP'
+            /**
+             * @ORM\Id
+             * @ORM\Column(type="integer")
+             * @ORM\GeneratedValue
+             * @Assert\NotBlank
+             * @Assert\Length(min=5)
+             */
+        PHP;
+
+        $db = $parser->parse($code);
+
+        $doctrineAnnotations = $db->getDoctrineLikeAnnotationTypes();
+
+        self::assertCount(5, $doctrineAnnotations);
+        self::assertEquals('ORM\Id', $doctrineAnnotations[0]);
+        self::assertEquals('ORM\Column', $doctrineAnnotations[1]);
+        self::assertEquals('ORM\GeneratedValue', $doctrineAnnotations[2]);
+        self::assertEquals('Assert\NotBlank', $doctrineAnnotations[3]);
+        self::assertEquals('Assert\Length', $doctrineAnnotations[4]);
     }
 }
